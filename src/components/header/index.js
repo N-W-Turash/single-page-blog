16baa910a6747d6aab149fb4a4e56780c298e5da
N-W-Turash <turash_sust@outlook.com>
2021-09-09 23:05:54 +0600

    feat: display single post and associated comments

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,14 @@
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Button } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+import { useHistory, useRouteMatch } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
 import "../../stylesheets/components/header/index.scss";
 
 function Header() {
+
+  let history = useHistory();
+  let match = useRouteMatch("/posts/:id");
+  
   return (
     <>
       <Navbar>
@@ -15,6 +21,18 @@ function Header() {
             />
             <h6 className="d-inline-block fw-bold ls-1">BLOG</h6>
           </Navbar.Brand>
+          {
+            match && match.isExact ?
+              <Button
+                variant="outline-secondary"
+                className="float-right btn-capsule px-4"
+                size="sm"
+                onClick={() => history.push("/")}
+              >
+                Back to all posts
+              </Button> : ("")
+          }
+
         </Container>
       </Navbar>
     </>
